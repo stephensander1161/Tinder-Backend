a35c41d54b97f5f063907455133d216555778e28
stephensander1161 <57675208+stephensander1161@users.noreply.github.com>
2021-03-01 17:43:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,69 +1,53 @@
-import express from 'express'
-import mongoose from 'mongoose'
+import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import Cards from './dbCards.js';
 import Cors from 'cors';
 
-
-//app config 
+//app config
 const app = express();
 dotenv.config();
 
-const PORT = process.env.PORT || 8001
+const PORT = process.env.PORT || 8001;
 const CONNECTION_URL = process.env.REACT_APP_DBCONSTRING;
-
-
-
-
 
 //middleware
 app.use(express.json());
 app.use(Cors());
 
+//db config
+mongoose.connect(process.env.REACT_APP_DBCONSTRING, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
 
-//db config 
-mongoose.connect(CONNECTION_URL, {
-    useNewUrlParser: true, 
-    useCreateIndex: true,
-    useUnifiedTopology: true,
+//api endpoints
 
-})
-
-
-
-//api endpoints 
-
-app.get('/', (req, res)=> res.status(200).send('hellow world'))
+app.get('/', (req, res) => res.status(200).send('hellow world'));
 
 app.post('/cards', (req, res) => {
-    const dbCard = req.body;
+	const dbCard = req.body;
 
-    Cards.create(dbCard, (err, data) => {
-        if (err)
-        {
-            res.status(500).send(err)
-
-        } else {
-            res.status(201).send(data)
-
-        }
-    })
+	Cards.create(dbCard, (err, data) => {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.status(201).send(data);
+		}
+	});
 });
 
 app.get('/cards', (req, res) => {
-    Cards.find((err, data) => {
-        if (err) {
-            res.status(500).send(err);
-        } else {
-            res.status(200).send(data);
-        }
-    });
+	Cards.find((err, data) => {
+		if (err) {
+			res.status(500).send(err);
+		} else {
+			res.status(200).send(data);
+		}
+	});
 });
-
-
-
-
 
 //listener
 
-app.listen(PORT, () => console.log(`listening on localhost: ${PORT}`))
+app.listen(PORT, () => console.log(`listening on localhost: ${PORT}`));
